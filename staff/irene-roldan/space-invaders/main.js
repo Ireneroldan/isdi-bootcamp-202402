f74ef76dfa7693df7781023fd76c0c3b6365f9f0
Irene <irene.roldan.montiel@gmail.com>
2024-02-18 19:19:31 +0100

    add modifications #166

--- staff/irene-roldan/space-invaders/main.js
+++ staff/irene-roldan/space-invaders/main.js
@@ -1,42 +1,63 @@
-var ship = document.getElementById("ship")
-var alien = document.querySelector("alien")
+var ship = document.getElementById("ship");
+var aliens = document.querySelectorAll(".alien");
 
-var x = 47
-var y = 90
-
+var x = 47;
+var y = 90;
 
 ship.style.left = x + "vw";
 ship.style.top = y + "vh";
 
+var alienX = 0;
+var alienY = 5;
+
+aliens.forEach(function (alien) {
+    var alienRect = alien.getBoundingClientRect();
+    if (alienX > 90) {
+        alienX = 0;
+        alienY += 10;
+    }
+    alien.style.left = alienX + "vw";
+    alien.style.top = alienY + "vh";
+    
+    alienX += 10;
+
+});
+
 
 document.onkeydown = function (event) {
     if (event.key === "ArrowLeft" && x > 0) 
-        x = x - 1
+        x = x - 1;
     else if (event.key === "ArrowRight" && x < 95)
-        x = x + 1
+        x = x + 1;
 
     if (event.key === "ArrowUp" && y > 0) 
-        y = y - 1
+        y = y - 1;
     else if (event.key === "ArrowDown" && y < 90)
-        y = y + 1
-    
+        y = y + 1;
+
     ship.style.left = x + "vw";
     ship.style.top = y + "vh";
 
-    var shipRect = ship.getBoundingClientRect()
-    var alienRect = alien.getBoundingClientRect()
+    // Verificar colisión con cada alien
+    aliens.forEach(function (alien) {
+        var alienRect = alien.getBoundingClientRect();
+        var shipRect = ship.getBoundingClientRect();
 
-    console.log(shipRect, alienRect);
+        if (
+            shipRect.x < alienRect.x + alienRect.width &&
+            shipRect.x + shipRect.width > alienRect.x &&
+            shipRect.y < alienRect.y + alienRect.height &&
+            shipRect.y + shipRect.height > alienRect.y
+        ) {
+            alien.src = "images/explosion.png";
+            ship.src = "images/explosion.png";
 
-    if (shipRect.x + alienRect.width > alienRect.x) {
-        ship.src = "images/explosion.png"
-        alien.src = "images/explosion.png"
+            setTimeout(function () {
+                ship.style.display = "none";
+                alien.style.display = "none";
+            }, 2000);
+        }
+    });
 
-        setTimeout(function () {
-            ship.style.display = "none"
-            alien.style.display = "none"
-        }, 2000);
-    }
-}
-
+};
 
